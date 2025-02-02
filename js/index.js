c5436711fde3ce8381e834f76f558b0bed565d00
Ghsjulian <ghsjulian@gmail.com>
2025-02-02 15:16:36 +0600

    working fine

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 const header = document.querySelector("header");
 const aside = document.querySelector("aside");
+const menu = document.querySelector(".menu-list");
 const nav = document.querySelector("#nav");
 const overly = document.querySelector("#overly");
+const main = document.querySelector("main");
+const logo = document.querySelector(".logo");
+const input = document.querySelector("input");
 
 overly.onclick = () => {
     aside.classList.toggle("mobile-menu");
@@ -11,3 +15,25 @@ nav.onclick = () => {
     aside.classList.toggle("mobile-menu");
     overly.classList.toggle("overly");
 };
+logo.onclick = () => {
+    main.innerHTML = `
+                <div class="page">
+                    <img src="./images/1.png" />
+                    <p>
+                        यदा यदा हि धर्मस्य ग्लानिर्भवति भारत ।
+                        अभ्युत्थानमधर्मस्य तदात्मानं सृजाम्यहम् ॥ परित्राणाय
+                        साधूनां विनाशाय च दुष्कृताम् ।
+                    </p>
+                </div>
+    `;
+};
+input.addEventListener("focus", () => {
+    menu.style.height = "78%";
+});
+input.addEventListener("keyup", e => {
+    const type = e.keyCode;
+    const value = e.target.value;
+    if (value) {
+        console.log(value);
+    }
+});
